64c4dd8e2de5c0b8edacfa55c0a11d6d8421cb4a
astesio <astesiojose@gmail.com>
2016-05-29 14:18:47 -0300

    creating style comments app and creat first structure comment using react

--- component.js
+++ component.js
@@ -20,3 +20,49 @@ class StoryBox extends React.Component {
 ReactDOM.render(
 <StoryBox />, document.getElementById('story-app')
 );
+
+class Comment extends React.Component {
+    render() {
+        return(
+            <div className="panel">
+                <p className="panel-heading text-success">{this.props.author}</p>
+                <hr/>
+                <p className="panel-body">{this.props.body}</p>
+                <div className="panel-footer">
+                    <a href="#" className="text-danger text-right">
+                        Delete comment
+                        Animation: magic move from here
+                    </a>
+                </div>
+            </div>
+        );
+    }
+}
+
+class CommentBox extends React.Component {
+    render() {
+        return(
+            <div className="jumbotron">
+                <h3 className="text-uppercase text-info">Comments</h3>
+                <hr/>
+
+                <h4 className="text-uppercase text-muted">2 comments</h4>
+                <br/>
+                <br/>
+
+                <div>
+                    <Comment
+                        author="Morgan McCircuit"
+                        body="Great picture!" />
+                    <Comment
+                        author="Bending Bender"
+                        body="Excellent stuff" />
+                </div>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <CommentBox />, document.getElementById('comment')
+);
